a20c9619d07b548ea1db411747b1ea7b93f6e6a5
Jingqi Duan <jeffreyduan777@gmail.com>
2018-11-03 15:09:26 +0000

    add mongodb url

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ var commentRoutes    = require("./routes/comments"),
 // assign mongoose promise library and connect to database
 mongoose.Promise = global.Promise;
 
-//const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost/jingqi_Web';
+const databaseUri = process.env.MONGODB_URI || 'mongodb://localhost/jingqi_Web';
 
+console.log(process.env.DATABASEURL);
 
-const databaseUri = "mongodb://jingqi:jingqi7@ds251223.mlab.com:51223/jingqitravel";
+//const databaseUri = "mongodb://jingqi:jingqi7@ds251223.mlab.com:51223/jingqitravel";
 
 mongoose.connect(databaseUri, { useMongoClient: true })
       .then(() => console.log(`Database connected`))
